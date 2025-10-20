7bea27ce4862e1652f9725e93e9a8c0f6110f4a1
Oscarrrrrrrrrrrrrrrr <oscaryavat3@gmail.com>
2025-10-20 11:59:41 +0700

    Update soal_11.sh

--- 11-20/soal_11.sh
+++ 11-20/soal_11.sh
@@ -1,49 +1,60 @@
 #!/bin/bash
 
-echo "Mengkonfigurasi Reverse Proxy Sirion..."
-
-apt update
-apt install -y nginx
-
-cat > /etc/nginx/sites-available/sirion.conf << 'EOF'
 server {
-    listen 5151;
-    server_name sirion.k-14.com www.k-14.com;
-    
-    # Path-based routing
+    listen 80;
+    server_name sirion.k14.com www.k14.com;
+
+    if ($host = sirion.k14.com) {
+        return 301 http://www.k14.com$request_uri;
+    }
+
     location /static/ {
-        proxy_pass http://10.15.43.32:5209/;
+        proxy_pass http://10.15.43.38/;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
     }
-    
-    location /app/ {
-        proxy_pass http://10.15.43.32:5526/;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-    }
-    
-    location / {
-        return 301 http://www.k-14.com$request_uri;
-    }
-}
 
-server {
-    listen 5151;
-    server_name _;
-    return 301 http://www.k-14.com$request_uri;
+    location /app/ {
+        proxy_pass http://10.15.43.39/;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        
+        rewrite ^/app/about(/.*)?$ /about$1 break;
+    }
+
+    location /admin {
+        auth_basic "Administrator Area";
+        auth_basic_user_file /etc/nginx/.htpasswd;
+        proxy_pass http://10.15.43.39/;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
+    location / {
+        return 200 '<!DOCTYPE html>
+        <html>
+        <head>
+            <title>War of Wrath: Lindon Bertahan</title>
+        </head>
+        <body>
+            <h1>War of Wrath: Lindon Bertahan</h1>
+            <p><a href="/static">Menuju Arsip Statis Lindon</a></p>
+            <p><a href="/app">Menuju Aplikasi Dinamis Vingilot</a></p>
+        </body>
+        </html>';
+        add_header Content-Type text/html;
+    }
 }
 EOF
 
-# Aktifkan konfigurasi
-ln -sf /etc/nginx/sites-available/sirion.conf /etc/nginx/sites-enabled/
-rm -f /etc/nginx/sites-enabled/default
+# Restart nginx
+systemctl restart nginx
+systemctl enable nginx
 
-# Start nginx manually (Docker environment)
-nginx -t && nginx
-
-echo "Reverse Proxy Sirion berhasil dikonfigurasi!"
+echo "Reverse proxy Sirion berhasil dikonfigurasi"
