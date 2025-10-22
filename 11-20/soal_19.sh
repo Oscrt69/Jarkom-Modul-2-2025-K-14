9f0ddf4c07b0a732e910ff2dad41c7b316985666
Arkanzahir <arkanzahir86@gmail.com>
2025-10-22 15:54:05 +0700

    Delete 11-20 directory

--- 11-20/soal_19.sh
+++
@@ -1,9 +0,0 @@
-nslookup havens.k14.com localhost
-
-curl -s -I http://havens.k14.com/ | head -n 3
-
-www_title=$(curl -s http://www.k14.com/ | grep -o '<title>[^<]*' | head -1)
-havens_title=$(curl -s http://havens.k14.com/ | grep -o '<title>[^<]*' | head -1)
-echo "www.k14.com title: $www_title"
-echo "havens.k14.com title: $havens_title"
-    
